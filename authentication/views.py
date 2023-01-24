a7d43d18ee8801519a95b44064647fb2a8d263d0
Yussef raouf <yussef.raouf11@gmail.com>
2023-01-24 21:12:34 +0200

    token date

--- authentication/views.py
+++ authentication/views.py
@@ -32,5 +32,6 @@ class LogoutView(APIView):
 
             return Response(status = status.HTTP_205_RESET_CONTENT)
         except Exception as e:
+            
             return Response(status = status.HTTP_400_BAD_REQUEST)
 
